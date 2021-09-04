ebafb29bfe498a04df194f4d7dc20f63452ef4e3
Filip Risteski <filip22risteski@gmail.com>
2021-09-04 13:42:44 +0200

    ver 0.2 ( Added Header )

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Sidebar />
+      <div className="app__body"></div>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import TuneIcon from "@material-ui/icons/Tune";
+import AppsIcon from "@material-ui/icons/Apps";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Emblem.png"
+          alt=""
+        />
+      </div>
+      <div className="header__middle">
+        {" "}
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <input placeholder="Search email..." type="text" />
+        <IconButton>
+          <TuneIcon />
+        </IconButton>
+      </div>
+
+      <div className="header__right">
+        <IconButton>
+          <HelpOutlineIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsIcon />
+        </IconButton>
+        <IconButton>
+          <AppsIcon />
+        </IconButton>
+        <IconButton>
+          <Avatar />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
